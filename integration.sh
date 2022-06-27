00859ad77685ec6b6cb974f2f280c32479fceae9
Aymen Mabrouk <amabrouk@edligo.com>
2022-06-27 16:11:58 +0100

    adding integration.sh script for the CI

--- integration.sh
+++ integration.sh
@@ -6,7 +6,7 @@ docker-compose up --detach
 # poll if db exists
 canConnect=false
 while [ "$canConnect"=false ] ; do
-	result=$(docker container exec -i mercato_db psql -h localhost -U postgres -d Training_DB -W admin -lqt | cut -f 1 -d \| | grep -e "Training_DB")
+	result=$(docker container exec -i db psql -h localhost -U postgres -d Training_DB -W admin -lqt | cut -f 1 -d \| | grep -e "Training_DB")
 	printf "."
  	if [[ "Training_DB"="$result" ]]
 	then	
@@ -19,11 +19,11 @@ done
 
 # insert 3 entities and print status code to stdout
 
-curl -s -o /dev/null -w "%{http_code}" -X POST -d '{"identifier" : "entity_1", "isactive" : true}' -H "Cache-Control: no-cache" -H "Content-Type: application/json" http://localhost:8000/api/Customer/GetCustomers
+curl -s -o /dev/null -w "%{http_code}" -X POST -d '{"firstName": "FName_testIntegration_1", "lastName": "LName_testIntegration_1", "address": "address_testIntegration_1", "phone": "58963741", "email": "testIntegration@gmail.com"}' -H "Cache-Control: no-cache" -H "Content-Type: application/json" http://localhost:8000/api/Customer/AddCustomer
 echo ""
-curl -s -o /dev/null -w "%{http_code}" -X POST -d '{"identifier" : "entity_2", "isactive" : true}' -H "Cache-Control: no-cache" -H "Content-Type: application/json" http://localhost:8000/api/Customer/GetCustomers
+curl -s -o /dev/null -w "%{http_code}" -X POST -d '{"firstName": "FName_testIntegration_2", "lastName": "LName_testIntegration_2", "address": "address_testIntegration_2", "phone": "58963741", "email": "testIntegration@gmail.com"}' -H "Cache-Control: no-cache" -H "Content-Type: application/json" http://localhost:8000/api/Customer/AddCustomer
 echo ""
-curl -s -o /dev/null -w "%{http_code}" -X POST -d '{"identifier" : "entity_3", "isactive" : true}' -H "Cache-Control: no-cache" -H "Content-Type: application/json" http://localhost:8000/api/Customer/GetCustomers
+curl -s -o /dev/null -w "%{http_code}" -X POST -d '{"firstName": "FName_testIntegration_3", "lastName": "LName_testIntegration_3", "address": "address_testIntegration_3", "phone": "58963741", "email": "testIntegration@gmail.com"}' -H "Cache-Control: no-cache" -H "Content-Type: application/json" http://localhost:8000/api/Customer/AddCustomer
 echo ""
 
 # get model values and print to stdout
